88a1c8f302f63dcfc684b1512a2421be3beec835
Johnson2903 <49341589+Johnson2903@users.noreply.github.com>
2019-06-09 15:08:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+  
+const secretUsername = 'johnson';
+const secretPassword = 'johnson123';
+document.querySelector(".signin").addEventListener('click' , checkValidation , {
+    once : true
+});
+
+function checkValidation(i){
+    i.preventDefault();
+    const usernameInput = document.getElementById("usernamesignup").value;
+    const passwordInput = document.getElementById("passwordsignup").value;
+
+    if(usernameInput === ''    || passwordInput === ''){
+
+    }
+
+    else if(usernameInput === secretUsername && passwordInput === secretPassword){
+        const successDiv = document.createElement('div');
+        successDiv.classList.add('divsuccess');
+        successDiv.textContent = "LOGIN SUCCESS !!";
+        document.querySelector('.signin').insertAdjacentElement('afterend' , successDiv);
+
+    }
+    
+    {
+        const errorDiv = document.createElement('div');
+        errorDiv.innerHTML = '<p>Incorrect <em>username</em> or <em>password</em></p><p>Try again</p>';
+        document.querySelector('.signin').insertAdjacentElement('afterend' , errorDiv);
+        errorDiv.classList.add('diverror');
+
+    };
+}
